da78249035098de93950a46559962a5d96201477
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 13:46:31 +0530

    addpost

--- src/component/Addpost.js
+++ src/component/Addpost.js
@@ -3,7 +3,26 @@ import React from 'react'
 const Addpost = () => {
   return (
     <div>
-        Addpost
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                    
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label for="" className="form-label">Title</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label for="" className="form-label">Message</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-primary">SUBMIT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
   )
