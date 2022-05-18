4f01df83aa807a223a437663f83e4c065608bf1e
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 13:27:53 +0530

    socialmedia app created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Addpost from './component/Addpost';
 
 function App() {
   return (
     <>
     <div>
-      app
+      <Addpost/>
     </div>
     </>
   );
